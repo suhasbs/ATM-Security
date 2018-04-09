d4105d5dd71a7cd680d34e7b72ca268315760883
Suhas <suhasbs.nitk@gmail.com>
2018-04-09 18:13:02 +0530

    Notifications added

--- mysite/AtmSecurity/urls.py
+++ mysite/AtmSecurity/urls.py
@@ -8,5 +8,9 @@ from django.conf import settings
 urlpatterns = [
     url(r'^test/$', views.test, name='test'),
     url(r'^image_upload/$', views.uploadImage, name='upload'),
+    url(r'^all_activity/$', views.showActivity, name='all_activity'),
 ]
+
+
+    
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 import AtmSecurity
+from django.views.static import serve
+
 from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^security/', include('AtmSecurity.urls'))
 ]
 
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
